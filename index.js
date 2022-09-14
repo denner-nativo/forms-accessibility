34233bd9b602e93ea57616658afc5482d06fbed8
Denner Portuguez <denner.portuguez@nativo.la>
2022-09-13 19:50:41 -0600

    Add erros, color constrast, grouping controls

--- index.js
+++ index.js
@@ -9,11 +9,21 @@ $(() => {
         $('#form-labels-1').addClass('was-validated');
         
     });
+    
+    $('#form-labels-2').on('submit', (e) => {
+        e.preventDefault()
+        if (!e.target.checkValidity()) {
+            $('#general-error-2').addClass('d-block');
+            e.stopPropagation()
+        }
+        $('#form-labels-2').addClass('was-validated');
+    });
 
     $('input').on('input', (e) => {
         if($('#form-labels-1')[0].checkValidity())
         {
             $('#general-error-1').removeClass('d-block');
+            $('#general-error-2').removeClass('d-block');
         }
     })
 })
